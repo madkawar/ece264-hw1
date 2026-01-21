27c84c4a8836d10e33f773b85a79f98a94de4c84
Madhura Sushrut Kawar <mkawar@eceprog5.ecn.purdue.edu>
2026-01-21 14:40:09 -0500

    initialize hw1

---
+++ sort.c
@@ -0,0 +1,8 @@
+/* MODIFY this file */
+
+#include "sort.h"
+
+void ssort(int * arr, int size) {
+	/* For step 3, fill this in to perform a selection sort
+	   For step 4, add conditional compilation flags to perform an ascending selection sort instead */
+}
